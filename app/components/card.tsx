356d46fb6c3117c68b3c290e723579b9e6566a27
Bogdan <bsshul@gmail.com>
2024-03-17 20:38:30 +0100

    Content editable

---
+++ app/components/card.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { ChangeEvent, useState } from "react";
+import ContentEditable from "react-contenteditable";
+
+type Props = {}
+
+export default function Card({ }: Props) {
+ const [title, setTitle] = useState<string>("");
+ const [body, setBody] = useState<string>("");
+
+ const handleTitleChange = (event: ChangeEvent<HTMLDivElement>) => {
+  setTitle(event.currentTarget.innerHTML);
+ };
+ const handleBodyChange = (event: ChangeEvent<HTMLDivElement>) => {
+  setBody(event.currentTarget.innerHTML);
+ };
+ return (
+  <article className="p-4 pb-5 w-full bg-green-100 rounded-md text-black">
+   <ContentEditable
+    html={title}
+    onChange={handleTitleChange}
+    className="outline-none font-semibold whitespace-nowrap"
+    data-placeholder="Title"
+   />
+   <ContentEditable
+    html={body}
+    onChange={handleBodyChange}
+    className="mt-4 outline-none"
+    data-placeholder="New note..."
+   />
+  </article>
+
+
+ )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
+import Card from "./components/card";
+
 export default function Home() {
   return (
-    <section className="h-dvh">
-      <h1 className='mb-12 text-3xl font-bold'>Page Transitions</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, sit
-        accusamus tenetur iure laboriosam ab dolor autem mollitia perspiciatis
-        enim est sint facere temporibus tempora dolorem. Laborum veniam
-        officiis eligendi.
-      </p>
+    <section>
+      <div className="text-lg grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <Card />
+      </div>
     </section>
   )
 }
